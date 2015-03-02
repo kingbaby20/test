4fe7ec432df0f4b8a41d128b44de6d5d6b71505c
root <root@ssd13.(none)>
2015-03-02 15:04:57 +0000

    test.c

---
+++ test.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/02/2015 03:00:07 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  _tl__ (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+    return 0;
+}
+
