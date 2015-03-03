aee07fbc7ec8de60be0b18d4889b847dd74d8a5a
root <root@ssd13.(none)>
2015-03-03 19:17:24 +0000

    test.c

--- test.c
+++ test.c
@@ -17,8 +17,41 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+void stringZip(const char *pSrc,long len,char *dst)
+{
+    const char *ptr = pSrc;
+    char *result = dst;
+    int k = 0;
+    char c = *ptr;
+    while(*ptr)
+    {
+        if(c==*ptr)
+        {
+            ptr++;
+            k++;
+        }
+        else
+        {
+            if(k>1)
+            *result++=(char)(k+'0');
+            *result++=c;
+            k=1;
+            c=*ptr++;
+        }
+    }
+    if(k>1)
+     *result++=(char)(k+'0');
+      *result++=c;
+      *result='\0';
+}
+
 int main(void)
 {
+    char src[] = "aaabbc******######ddde";
+    char dst[256] = {0};
+    stringZip(src,strlen(src),dst);
+    puts(dst);
     printf("hello world1\n");
     printf("hello world2\n");
     return 0;
